7e67b40b31900a948db0db94788bb17976ec316f
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-03-24 14:05:15 -0300

    Ajustes

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import {FaPizzaSlice} from 'react-icons/fa'
+import {VscSignIn} from 'react-icons/vsc'
 import Header from "../lib/src/components/Header";
 import Input from "../lib/src/components/Input";
+import Text from "../lib/src/components/Text";
 
 export default function LoginScreen() {
+    const [action, setAction] = useState(false)
+
     return (
         <>
             <Header className={""}>
@@ -18,30 +22,29 @@ export default function LoginScreen() {
                         <form className="login-form" action="index.html">
                             <h3 className="login-head"><i className="fa fa-lg fa-fw fa-user" />SIGN IN</h3>
                             <div className="form-group">
-                                <label className="control-label">USERNAME</label>
-                                <Input className="form-control" type="text" placeholder="Email" autoFocus />
+                                <label className="control-label text-uppercase">Email</label>
+                                <Input getValue={() => {}} right onAction={action} className="form-control" type="text" placeholder="Email" autoFocus />
                             </div>
                             <div className="form-group">
-                                <label className="control-label">PASSWORD</label>
+                                <label className="control-label text-uppercase">Senha</label>
                                 <Input className="form-control" type="password" placeholder="Password"/>
                             </div>
+                            <div className="form-group btn-container">
+                                <button className="btn btn-primary btn-block">
+                                    <VscSignIn size={30} />Entrar
+                                </button>
+                            </div>
+                            <br />
                             <div className="form-group">
                                 <div className="utility">
-                                    <div className="animated-checkbox">
-                                        <label>
-                                            <input type="checkbox" />
-                                                <span className="label-text">Stay Signed in</span>
-                                        </label>
-                                    </div>
+                                    <label>
+                                        <a href="#" className="label-text">Cadastrar-se?</a>
+                                    </label>
+                                    <Text value="ou"/>
                                     <p className="semibold-text mb-2">
-                                        <a href="#">Forgot Password?</a>
+                                        <a href="#">Esqueceu a senha?</a>
                                     </p>
                                 </div>
-                            </div>
-                            <div className="form-group btn-container">
-                                <button className="btn btn-primary btn-block"><i
-                                    className="fa fa-sign-in fa-lg fa-fw"/>SIGN IN
-                                </button>
                             </div>
                         </form>
                     </div>
